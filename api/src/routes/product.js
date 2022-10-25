8ea4d6fc24f6f0fdd4a266781e2e5faa31b93945
osmomysl <dannvx@gmail.com>
2022-10-25 10:17:38 +0300

    move helpers into separate file

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,51 +1,6 @@
 import bodyParser from 'body-parser'
 import {Router} from 'express'
-import {productStripFields} from '../../../common/helpers.js'
-
-function ensureFields(body) {
-    const fields = productStripFields(body)
-
-    const errors = {}
-    if ('isInSale' in fields && 'boolean' !== typeof fields.isInSale) errors.isInSale = [new TypeError("'isInSale' must be boolean")]
-    if ('name' in fields && 'string' !== typeof fields.name) errors.name = [new TypeError("'name' must be a string")]
-    if ('itemInitial' in fields && 'string' !== typeof fields.itemInitial) errors.itemInitial = [new TypeError("'name' must be a string")]
-
-    if (Object.keys(errors).length) return {fields: null, errors}
-    return {fields}
-}
-
-function ensureFieldsCreate(body) {
-    if (!('isInSale' in body)) return {fields: null, errors: {isInSale: m.FieldMissing.create("'isInSale' must be specified")}}
-    return ensureFields(body)
-}
-
-function ensureFieldsUpdate(body) {
-    const fields = productStripFields(body)
-    if (!Object.keys(fields).length) return {fields: null, errors: m.FieldMissing.create("at least one of the fields must be specified")}
-
-    return ensureFields(body)
-}
-
-function handleUpdateMissingFields(e, req, res, next) {
-    if (!e) return next()
-    if (!('code' in e) || m.FieldMissing.code !== e.code) return next(e)
-    res.status(400).json(e)
-}
-
-function makeEnsureFields(ensureFields) {
-    return (req, res, next) => {
-        const _res = ensureFields(req.body)
-        console.log("makeEnsureFields-produced method, ensureFields _res:", _res)
-
-        if (!_res.fields) {
-            if (!_res.errors) return next(new Error("ensureFieldsCreate must return either fields or errors"))
-            return next(_res.errors)
-        }
-
-        req.body.fields = _res.fields
-        next()
-    }
-}
+import {makeEnsureFields, ensureFieldsCreate, ensureFieldsUpdate} from './product-helpers.js'
 
 function product(product) {
     const router = Router()
