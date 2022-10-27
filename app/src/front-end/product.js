4a6c2779c597c55074dcf14b17b3d9bc732ccf10
osmomysl <dannvx@gmail.com>
2022-10-27 10:11:58 +0300

    request the created product

--- app/src/front-end/product.js
+++ app/src/front-end/product.js
@@ -12,17 +12,28 @@ function send(data) {
     })
 }
 
-function success(res) {
-    // return fetch(`api/product/${res.body}`, {method: 'GET'})
-    // .then(res => {
-    //     return res.json()
-    // }).then(body => {
-    //     console.log("got the written product:", body)
-    // })
+function successCreate(res) {
+    return fetch(`api/product/${res.body}`, {method: 'GET'})
+    .then(res => {
+        return res.json().then((body) => {
+            return {res, body}
+        })
+    }).then(res => {
+        console.log("parsed body of response to GET request:", res.body);
+        return (res.res.ok) ? successRead(res) : failureRead(res)
+    })
 }
 
-function failure(res) {
-    return Promise.resolve()
+function failureCreate(res) {
+    console.log("something went wrong creating the product", res);
+}
+
+function successRead(res) {
+    console.log("got the written product:", res.body)
+}
+
+function failureRead(res) {
+    console.log("something went wrong getting the written product:", res);
 }
 
 function main() {
@@ -42,7 +53,7 @@ function main() {
     }).then((res) => {
         console.log('parsed response body', res.body)
 
-        return (res.res.ok) ? success(res) : failure(res)
+        return (res.res.ok) ? successCreate(res) : failureCreate(res)
     })
 }
 
